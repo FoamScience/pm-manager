406ddcd5caa162e94691a18c9010f9386c759301
Elwardi <elwardifadeli@gmail.com>
2020-04-22 15:56:15 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+const { Text, Password } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { StaticApp } = require('@keystonejs/app-static');
+
+const TodoSchema = require('./lists/Todo.js');
+const UserSchema = require('./lists/User.js');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+
+
+const keystone = new Keystone({
+  name: 'Phone Book Manager',
+  adapter: new MongooseAdapter(),
+  appVersion: {
+    version: '0.0.1',
+    addVersionToHttpHeaders: true,
+    access: true,
+  },
+});
+
+//keystone.createList('Todo', TodoSchema);
+Numbers = keystone.createList ( 'Number', TodoSchema);
+Users = keystone.createList('User', UserSchema);
+
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+  config: {
+    identityField: 'username', // default: 'email'
+    secretField: 'password', // default: 'password'
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    //new StaticApp({ path: '/', src: 'public' , authStrategy}),
+    // Setup the optional Admin UI
+    new AdminUIApp({ authStrategy, enableDefaultRoute: true }),
+  ]
+};
---
+++ lists/User.js
@@ -0,0 +1,31 @@
+const { Text, Password, Checkbox } = require('@keystonejs/fields');
+
+module.exports = {
+  adminConfig: {
+    defaultColumns: 'username,email',
+    defaultPageSize: 50,
+    defaultSort: 'email',
+    maximumPageSize: 100,
+  },
+  fields: {
+    username: {
+      type: Text,
+      isRequired: true,
+	  isUnique: true,
+    },
+    password: {
+      type: Password,
+      isRequired: true,
+    },
+	isAdmin: { type: Checkbox, defaultValue: false },
+	email: {
+      type: Text,
+      // 2. Only authenticated users can read/update their own email, not any other user's.
+      // Admins can read/update anyone's email.
+      access: ({ existingItem, authentication: { item } }) => {
+        return item.isAdmin || existingItem.id === item.id;
+      },
+    },
+  },
+  labelField: 'username',
+};
